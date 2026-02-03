1c9e652d4963b7bc707382964bba5d9bcbea11aa
Jakob Landsem <jakob.landsem@gmail.com>
2026-02-03 12:16:17 +0100

    test

--- src/main/java/ntnu/gruppe21/Main.java
+++ src/main/java/ntnu/gruppe21/Main.java
@@ -2,6 +2,6 @@ package ntnu.gruppe21;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("If you read this, you are gay");
     }
 }
