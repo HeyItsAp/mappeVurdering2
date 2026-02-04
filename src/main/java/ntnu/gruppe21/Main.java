6b0e32fe4dd0e3f312bc3357f56315f01aa8de6d
Jakob Landsem <jakob.landsem@gmail.com>
2026-02-04 13:05:52 +0100

    Added necessary dependencies and plugins to the pom.xml file. Also made git ignore the targetr folder.

--- src/main/java/ntnu/gruppe21/Main.java
+++ src/main/java/ntnu/gruppe21/Main.java
@@ -1,7 +1,7 @@
 package ntnu.gruppe21;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("If you read this, you are gay");
-    }
+  public static void main(String[] args) {
+    System.out.println("If you read this, you are gay");
+  }
 }
