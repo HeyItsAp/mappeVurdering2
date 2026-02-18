57b4026691dd7a35d8850f4937056aa3ccfcc815
Jakob Landsem <jakob.landsem@gmail.com>
2026-02-18 20:12:53 +0100

    Correcting Javadoc

--- src/main/java/ntnu/gruppe21/calculators/TransactionCalculator.java
+++ src/main/java/ntnu/gruppe21/calculators/TransactionCalculator.java
@@ -11,28 +11,28 @@ public interface TransactionCalculator {
   /**
    * Calculates the gross value of the share.
    *
-   * @return The gross value
+   * @return The gross value.
    */
   public BigDecimal calculateGross();
 
   /**
    * Calculates the commission fee of the transaction.
    *
-   * @return The amount that need to be paid of commission
+   * @return The amount that need to be paid of commission.
    */
   public BigDecimal calculateCommission();
 
   /**
    * Calculates the tax that need to be paid to the government.
    *
-   * @return The tax
+   * @return The tax.
    */
   public BigDecimal calculateTax();
 
   /**
-   * Calculates the total price of the transaction, including commision and tax.
+   * Calculates the total price of the transaction, including commission and tax.
    *
-   * @return The amount to be paid
+   * @return The value of the transaction.
    */
   public BigDecimal calculateTotal();
 }
