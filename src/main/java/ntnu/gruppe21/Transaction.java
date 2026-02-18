9ec96fe23f2893fef8cf6750e19af4b6e2504437
Jakob Landsem <jakob.landsem@gmail.com>
2026-02-18 14:26:31 +0100

    Added the abstract class Transaction.

---
+++ src/main/java/ntnu/gruppe21/Transaction.java
@@ -0,0 +1,32 @@
+package ntnu.gruppe21;
+
+public abstract class Transaction {
+    private final Share share;
+    private final int week;
+    private final TransactionCalculator calculator;
+    private boolean committed = false;
+
+    protected Transaction(Share share, int week, TransactionCalculator calculator) {
+        this.share = share;
+        this.week = week;
+        this.calculator = calculator;
+    }
+
+    public Share getShare() {
+        return share;
+    }
+
+    public int getWeek() {
+        return week;
+    }
+
+    public TransactionCalculator getCalculator() {
+        return calculator;
+    }
+
+    public boolean isCommitted() {
+        return committed;
+    }
+
+    public abstract void commit(Player player);
+}
