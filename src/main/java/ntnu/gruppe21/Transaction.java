e7332afa6edcf061320ae2df5487ce95a6e4ccdc
Jakob Landsem <jakob.landsem@gmail.com>
2026-02-18 14:33:36 +0100

    Added javadocs and formatting

--- src/main/java/ntnu/gruppe21/Transaction.java
+++ src/main/java/ntnu/gruppe21/Transaction.java
@@ -1,32 +1,40 @@
 package ntnu.gruppe21;
 
+import ntnu.gruppe21.calculators.TransactionCalculator;
+
+/** Abstract class representing a single transaction. */
 public abstract class Transaction {
-    private final Share share;
-    private final int week;
-    private final TransactionCalculator calculator;
-    private boolean committed = false;
+  private final Share share;
+  private final int week;
+  private final TransactionCalculator calculator;
+  private boolean committed = false;
 
-    protected Transaction(Share share, int week, TransactionCalculator calculator) {
-        this.share = share;
-        this.week = week;
-        this.calculator = calculator;
-    }
+  protected Transaction(Share share, int week, TransactionCalculator calculator) {
+    this.share = share;
+    this.week = week;
+    this.calculator = calculator;
+  }
 
-    public Share getShare() {
-        return share;
-    }
+  public Share getShare() {
+    return share;
+  }
 
-    public int getWeek() {
-        return week;
-    }
+  public int getWeek() {
+    return week;
+  }
 
-    public TransactionCalculator getCalculator() {
-        return calculator;
-    }
+  public TransactionCalculator getCalculator() {
+    return calculator;
+  }
 
-    public boolean isCommitted() {
-        return committed;
-    }
+  /**
+   * Checks if the transaction is comitted.
+   *
+   * @return True if committed, false if not
+   */
+  public boolean isCommitted() {
+    return committed;
+  }
 
-    public abstract void commit(Player player);
+  public abstract void commit(Player player);
 }
