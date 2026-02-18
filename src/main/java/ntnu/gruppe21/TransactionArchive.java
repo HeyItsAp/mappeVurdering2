44689a146d09a284c25e04a9ec7bb29a797a16b8
Jakob Landsem <jakob.landsem@gmail.com>
2026-02-18 20:42:11 +0100

    Formatting

--- src/main/java/ntnu/gruppe21/TransactionArchive.java
+++ src/main/java/ntnu/gruppe21/TransactionArchive.java
@@ -1,100 +1,86 @@
 /**
- * An archive of transactions, which can be used to store and retrieve transactions based on the week they were made.
- * 
+ * An archive of transactions, which can be used to store and retrieve transactions based on the
+ * week they were made.
+ *
  * @author Adrian
  */
-
 package ntnu.gruppe21;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class TransactionArchive {
-    /**
-     * An list of transactions, which can contain both purchases and sales.
-     */
-    private List<Transaction> transactions;
+  /** An list of transactions, which can contain both purchases and sales. */
+  private List<Transaction> transactions;
 
+  /** Creates a new TransactionArchive with an empty list of transactions. */
+  public TransactionArchive() {
+    this.transactions = new ArrayList<>();
+  }
 
-    /**
-     * Creates a new TransactionArchive with an empty list of transactions.
-     */
-    public TransactionArchive() {
-        this.transactions = new ArrayList<>();
-    }
+  /**
+   * Adds a transaction to the archive.
+   *
+   * @param transaction The transaction to be added to the archive.
+   * @return true if the transaction was added successfully.
+   */
+  public boolean add(Transaction transaction) {
+    transactions.add(transaction);
+    return true;
+  }
 
-    /**
-     * Adds a transaction to the archive.
-     * 
-     * @param transaction The transaction to be added to the archive.
-     * @return true if the transaction was added successfully.
-     */
-    public boolean add(Transaction transaction) {
-        transactions.add(transaction);
-        return true;
-    }
+  /**
+   * Checks if the archive is empty.
+   *
+   * @return true/false based on whether the archive is empty or not.
+   */
+  public boolean isEmpty() {
+    return transactions.isEmpty();
+  }
 
-    /**
-     * Checks if the archive is empty.
-     * 
-     * @return true/false based on whether the archive is empty or not.
-     */
-    public boolean isEmpty() {
-        return transactions.isEmpty();
-    }
+  /**
+   * Returns a list of transactions that were made in the specified week.
+   *
+   * @param week The week in question
+   * @return A list of transactions
+   */
+  public List<Transaction> getTransactionsInWeek(int week) {
+    List<Transaction> filteredTransactions =
+        transactions.stream().filter(t -> t.getWeek() == week).toList();
+    return filteredTransactions;
+  }
 
-    /**
-     * Returns a list of transactions that were made in the specified week.
-     * 
-     * @param week The week in question
-     * @return A list of transactions 
-     */
-    public List<Transaction> getTransactionsInWeek(int week) {
-        List<Transaction> filteredTransactions = transactions.stream()
-                .filter(t -> t.getWeek() == week)
-                .toList();
-        return filteredTransactions;
-    }
-    
-    /**
-     * Returns a list of purchases, that were made in the specified week.
-     * 
-     * @param week The week in question
-     * @return Thelist of purchases
-     */
-    public List<Purchase> getPurchases(int week) {
-        List<Purchase> filteredTransactions = transactions.stream()
-                .map(t -> (Purchase) t)
-                .filter(t -> t.getWeek() == week)
-                .toList();
-        return filteredTransactions;
-    }
-    
-    /**
-     * Returns a list of sales, that were made in the specified week.
-     * 
-     * @param week The week in question
-     * @return Thelist of sales
-     */
-    public List<Sale> getSales(int week) {
-        List<Sale> filteredTransactions = transactions.stream()
-                .map(t -> (Sale) t)
-                .filter(t -> t.getWeek() == week)
-                .toList();
-        return filteredTransactions;
-    }
+  /**
+   * Returns a list of purchases, that were made in the specified week.
+   *
+   * @param week The week in question
+   * @return Thelist of purchases
+   */
+  public List<Purchase> getPurchases(int week) {
+    List<Purchase> filteredTransactions =
+        transactions.stream().map(t -> (Purchase) t).filter(t -> t.getWeek() == week).toList();
+    return filteredTransactions;
+  }
 
-    /**
-     * Counts the number of distinct weeks in which transactions were made.
-     * 
-     * @return the length of the list of distinct list, which is the number of distinct weeks.
-     */
-    public int countDistinctWeeks(){
-        List<Integer> weeks = transactions.stream()
-                .map(t -> t.getWeek())
-                .distinct()
-                .toList();
-        return weeks.size();
-    }
+  /**
+   * Returns a list of sales, that were made in the specified week.
+   *
+   * @param week The week in question
+   * @return Thelist of sales
+   */
+  public List<Sale> getSales(int week) {
+    List<Sale> filteredTransactions =
+        transactions.stream().map(t -> (Sale) t).filter(t -> t.getWeek() == week).toList();
+    return filteredTransactions;
+  }
+
+  /**
+   * Counts the number of distinct weeks in which transactions were made.
+   *
+   * @return the length of the list of distinct list, which is the number of distinct weeks.
+   */
+  public int countDistinctWeeks() {
+    List<Integer> weeks = transactions.stream().map(t -> t.getWeek()).distinct().toList();
+    return weeks.size();
+  }
 }
--- src/main/java/ntnu/gruppe21/TransactionException.java
+++ src/main/java/ntnu/gruppe21/TransactionException.java
@@ -1,22 +1,17 @@
 package ntnu.gruppe21;
 
-/**
- * Thrown to indicate a transaction has failed.
- */
-public class TransactionException extends Exception{
+/** Thrown to indicate a transaction has failed. */
+public class TransactionException extends Exception {
 
-    /**
-     * Constructs a {@code TransactionException} with no detail message.
-     */
-    public TransactionException() {}
+  /** Constructs a {@code TransactionException} with no detail message. */
+  public TransactionException() {}
 
-    /**
-     * Constructs an {@code TransactionException} with the
-     * specified detail message.
-     *
-     * @param message the detail message.
-     */
-    public TransactionException(String message) {
-        super(message);
-    }
+  /**
+   * Constructs an {@code TransactionException} with the specified detail message.
+   *
+   * @param message the detail message.
+   */
+  public TransactionException(String message) {
+    super(message);
+  }
 }
