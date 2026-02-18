fbd21c5d2933c0e015e4a4d13ed088f210051870
Jakob Landsem <jakob.landsem@gmail.com>
2026-02-18 20:11:52 +0100

    Created the exception TransactionException.

---
+++ src/main/java/ntnu/gruppe21/TransactionException.java
@@ -0,0 +1,22 @@
+package ntnu.gruppe21;
+
+/**
+ * Thrown to indicate a transaction has failed.
+ */
+public class TransactionException extends RuntimeException{
+
+    /**
+     * Constructs a {@code TransactionException} with no detail message.
+     */
+    public TransactionException() {}
+
+    /**
+     * Constructs an {@code TransactionException} with the
+     * specified detail message.
+     *
+     * @param message the detail message.
+     */
+    public TransactionException(String message) {
+        super(message);
+    }
+}
