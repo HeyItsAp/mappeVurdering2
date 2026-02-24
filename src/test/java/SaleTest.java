fff36508fbb452739b85112ad41b57ed13673c07
AdrianBalunan <adrianbalunan63@gmail.com>
2026-02-25 00:18:45 +0100

    Feature: JavaDoc ShareTest

--- src/test/java/SaleTest.java
+++ src/test/java/SaleTest.java
@@ -12,10 +12,18 @@ import java.math.BigDecimal;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * Tests for the Sale class.
+ * 
+ * The tests currently focus on basic functionality and expected behavior of the Sale class.
+ * @author Adrian Balunan
+ */
+
 public class SaleTest {
     private Transaction sale;
     private Player player1;
 
+    /* Method to set up a Sale instance before each test case is executed, ensuring a consistent test environment. */
     @BeforeEach
     public void setup(){
         Stock stock1 = new Stock("Bit", "Company1", new BigDecimal(1000));
@@ -24,6 +32,7 @@ public class SaleTest {
         sale = new Sale(share, 1);
     }
 
+    /* Method to verify that the commit() method correctly commits a sale transaction and that it cannot be committed more than once. */
     @Test
     public void commitingWorks() throws TransactionException {
         sale.commit(player1);
