696e04d1bac10b47ff69675d606cc3641167cb59
AdrianBalunan <adrianbalunan63@gmail.com>
2026-02-24 23:40:34 +0100

    Feature: Sale class unit testing

---
+++ src/test/java/SaleTest.java
@@ -0,0 +1,37 @@
+import ntnu.gruppe21.Player;
+import ntnu.gruppe21.Share;
+import ntnu.gruppe21.Stock;
+import ntnu.gruppe21.transaction.Sale;
+import ntnu.gruppe21.transaction.Transaction;
+import ntnu.gruppe21.transaction.TransactionException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SaleTest {
+    private Transaction sale;
+    private Share share;
+    private Player player1;
+
+    @BeforeEach
+    public void setup(){
+        Stock stock1 = new Stock("Bit", "Company1", new BigDecimal(1000));
+        share = new Share(stock1, new BigDecimal(10), stock1.getSalesPrice());
+        player1 = new Player("Name", new BigDecimal(100000));
+        sale = new Sale(share, 1);
+    }
+
+    @Test
+    public void commitingWorks() throws TransactionException {
+        sale.commit(player1);
+        assertTrue(sale.isCommitted());
+        assertThrows(TransactionException.class, () -> {
+            sale.commit(player1);
+        });
+    }
+
+}
