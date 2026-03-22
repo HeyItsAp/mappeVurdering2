56ae63865f13af25f5a9e4a1354daf1c8ac43740
AdrianBalunan <adrianbalunan63@gmail.com>
2026-03-22 16:15:58 +0100

    Test: Testing it on main here

--- src/main/java/ntnu/gruppe21/Main.java
+++ src/main/java/ntnu/gruppe21/Main.java
@@ -11,5 +11,9 @@ public class Main {
 
     Exchange exchange = Filehandler.getExhangeData();
     System.out.println(exchange.getName() + ". Week: " + exchange.getWeek() + ", " + exchange.getStock("NVDA").getSymbol() + ": " + exchange.getStock("NVDA").getSalesPrice());
+
+    Exchange exchange1 = Filehandler.getSaveData(Filehandler.saveExchangeData(exchange));
+    System.out.println(exchange1.getName() + ". Week: " + exchange1.getWeek() + ", " + exchange1.getStock("NVDA").getSymbol() + ": " + exchange1.getStock("NVDA").getSalesPrice());
+
   }
 }
