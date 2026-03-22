3664c7339487bc44664087a03f9adbfa8a492ac3
AdrianBalunan <adrianbalunan63@gmail.com>
2026-03-22 16:14:03 +0100

    Feat: Own Method for getting data from save file

--- src/main/java/ntnu/gruppe21/filehandler/Filehandler.java
+++ src/main/java/ntnu/gruppe21/filehandler/Filehandler.java
@@ -14,9 +14,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Filehandler {
-    public static void saveExchangeData(Exchange exchange){
+    public static String saveExchangeData(Exchange exchange){
+        String filename = "savaData" + exchange.getName() + exchange.getWeek();
         try {
-            PrintWriter pw = new PrintWriter("src/main/resources/Exchanges/saveData" + exchange.getName() + exchange.getWeek() + ".csv");
+            PrintWriter pw = new PrintWriter("src/main/resources/saves/" + filename + ".csv");
             pw.write("# Save on Exchange: " + exchange.getName() + ", Week: " + exchange.getWeek());
             pw.write("# Ticker,Name,{Prices}");
             pw.write(" ");
@@ -30,11 +31,11 @@ public class Filehandler {
                 }
             );
 
-
         } catch (Exception e) {
             e.printStackTrace();
         }
-
+        System.out.println("Attempt to save file: " + filename + ".csv; At resources/saves");
+        return filename;
     }
     public static Exchange getExhangeData(){
         String csvfile = "src/main/resources/Exchanges/exchangeData.csv";
@@ -71,5 +72,47 @@ public class Filehandler {
         }
         Exchange exhange = new Exchange("ExhangeFromFile", listOfStocks);
         return exhange;
+    }
+    public static Exchange getSaveData (String filename){
+        String csvfile = "src/main/resources/saves/" + filename + ".csv";
+        String line = "";
+        List<Stock> listOfStocks = new ArrayList<>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(csvfile));
+            while ((line = br.readLine()) != null){
+                String trimmedLine = line.trim();
+
+                if (trimmedLine.isEmpty()){
+                    continue;
+                }
+                if(trimmedLine.startsWith("#")){
+                    continue;
+                }
+
+                String[] values = trimmedLine.split(",");
+
+                // Just a normal print out
+                for (String value : values){
+                    System.out.print(value.trim() + " ");
+                }
+                System.out.print("\n");
+
+                String[] savedPricesString = values[2].split(";");
+                List<BigDecimal> savedPrices = Arrays
+                        .stream(savedPricesString)
+                        .map(BigDecimal::new)
+                        .toList();
+                Stock stock = new Stock(values[0], values[1], savedPrices.getFirst());
+                for (int i = 1; i < savedPrices.size(); i++){
+                    stock.addNewSalesPrice(savedPrices.get(i));
+                }
+                listOfStocks.add(stock);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Exchange exhange = new Exchange("Save:" + filename, listOfStocks);
+        return exhange;
+
     }
 }
