12f22c9578927ae82c370cba535aafa9bda3c385
Jakob Landsem <jakob.landsem@gmail.com>
2026-04-24 13:49:43 +0200

    Feat/status (#21)
    * small fixes
    
    * Added methods calculating net worth.
    
    * annoying thing
    
    * Implemented status retrieval
    
    * skriveleif

--- src/test/java/PortfolioTest.java
+++ src/test/java/PortfolioTest.java
@@ -17,31 +17,29 @@ import org.junit.jupiter.api.Test;
  *
  * @author Adrian Balunan
  */
-public class PortofolioTest {
-  private Portfolio protofoilo;
+public class PortfolioTest {
+  private Portfolio portfolio;
   private Stock stock;
 
   /* Method to set up a Portfolio instance before each test case is executed, ensuring a consistent test environment. */
   @BeforeEach
   public void setup() {
     stock = new Stock("Bit", "ExampleCompany", new BigDecimal(20000));
-    // Share shareOfStock = new Share(stock, new BigDecimal(1000), new BigDecimal(2531));
-    // Share share2OfStock = new Share(stock, new BigDecimal(2000), new BigDecimal(5062));
-    protofoilo = new Portfolio();
+    portfolio = new Portfolio();
   }
 
   /* Method to verify that the getShares() method returns an empty list when no shares are added to the portfolio. */
   @Test
   public void gettingSharesShouldWork() {
-    assertEquals(new ArrayList<>(), protofoilo.getShares());
+    assertEquals(new ArrayList<>(), portfolio.getShares());
   }
 
   /* Method to verify that the addShare() method correctly adds a share to the portfolio and that the getShares() method returns the correct list of shares. */
   @Test
   public void addingSharesShouldWork() {
     Share shareOfStock = new Share(stock, new BigDecimal(1000), new BigDecimal(2531));
-    protofoilo.addShare(shareOfStock);
-    assertEquals(new ArrayList<>(List.of(shareOfStock)), protofoilo.getShares());
+    portfolio.addShare(shareOfStock);
+    assertEquals(new ArrayList<>(List.of(shareOfStock)), portfolio.getShares());
   }
 
   /* Method to verify that the removeShare() method correctly removes a share from the portfolio and that the getShares() method returns the correct list of shares. */
@@ -49,17 +47,17 @@ public class PortofolioTest {
   public void removiingSharesShouldWork() {
     Share shareOfStock = new Share(stock, new BigDecimal(1000), new BigDecimal(2531));
     Share share2OfStock = new Share(stock, new BigDecimal(2000), new BigDecimal(5062));
-    protofoilo.addShare(shareOfStock);
-    protofoilo.addShare(share2OfStock);
-    protofoilo.removeShare(shareOfStock);
-    assertEquals(new ArrayList<>(List.of(share2OfStock)), protofoilo.getShares());
+    portfolio.addShare(shareOfStock);
+    portfolio.addShare(share2OfStock);
+    portfolio.removeShare(shareOfStock);
+    assertEquals(new ArrayList<>(List.of(share2OfStock)), portfolio.getShares());
   }
 
   /* Method to verify that the containsShare() method correctly identifies whether a share is present in the portfolio. */
   @Test
   public void containSearchShouldWork() {
     Share shareOfStock = new Share(stock, new BigDecimal(1000), new BigDecimal(2531));
-    protofoilo.addShare(shareOfStock);
-    assertTrue(protofoilo.containsShare(shareOfStock));
+    portfolio.addShare(shareOfStock);
+    assertTrue(portfolio.containsShare(shareOfStock));
   }
 }
