32c0fff1742c8ad1fdf6e86153b7b0ee5bc7e178
AdrianBalunan <adrianbalunan63@gmail.com>
2026-05-13 11:06:19 +0200

    Feat: SaveManager.java manages folderSave and uses methods from the pervious filehandlers

---
+++ src/main/java/ntnu/gruppe21/filehandler/SaveManager.java
@@ -0,0 +1,66 @@
+package ntnu.gruppe21.filehandler;
+
+import ntnu.gruppe21.Exchange;
+import ntnu.gruppe21.Player;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * This class implements both Filehandlers {@link FilehandlerExchange} and {@link FilehandlerPlayer}
+ * to manage saves by invoking static methods from both.
+ *
+ * <p>
+ *     Mainly handles folders, creating them and places saved csv data into those folders.
+ *     When loading, it uses the static methods from {@link FilehandlerPlayer} and {@link FilehandlerExchange}
+ *     to return objects
+ * </p>
+ *
+ */
+public class SaveManager {
+    private static final String saves_root = "src/main/resources/saves";
+    private final String folderPath;
+
+    /**
+     * Constructor.
+     *
+     * @param saveName points to folder name containing csv files.
+     */
+    public SaveManager(String saveName){
+        this.folderPath = saves_root + saveName;
+    }
+
+    /**
+     * Performs a full save of the exchange and player data. Creates folder, csv files through static methods
+     * and then prints out.
+     *
+     * @param player
+     * @param exchange
+     * @throws Exception
+     */
+    public void save(Player player, Exchange exchange) throws Exception {
+        Files.createDirectories(Paths.get(folderPath));
+        FilehandlerPlayer.savePlayerData(player, folderPath);
+        FilehandlerExchange.saveExchangeData(exchange, folderPath);
+        System.out.println("Game saved to: " + folderPath);
+    }
+
+    /**
+     * Reads and returns Player object through static method.
+     *
+     * @return Player object
+     */
+    public Player loadPlayer() {
+        return FilehandlerPlayer.getPlayerSavedData(folderPath);
+    }
+
+
+    /**
+     * Reads and returns Exchange object through static method.
+     *
+     * @return Exchange object
+     */
+    public Exchange loadExchange() {
+        return FilehandlerExchange.getSaveData(folderPath);
+    }
+}
