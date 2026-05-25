972632db5363ffdce31844d0c5904ea4fd8cb4d7
Jakob Landsem <jakob.landsem@gmail.com>
2026-05-25 15:28:17 +0200

    Were some weird problem with the temporary test code, so ive commented it out

--- src/main/java/ntnu/gruppe21/Main.java
+++ src/main/java/ntnu/gruppe21/Main.java
@@ -13,30 +13,31 @@ import ntnu.gruppe21.view.Screen;
 public class Main extends Application {
   public static void main(String[] args) {
 
-    // Fragmentry Simulation for periodic/random save after some actions:
-    Player player = new Player.Builder("Adrian", new BigDecimal(200000), Difficulty.EASY).build();
-    Exchange exchange = FilehandlerExchange.getExchangeDataset("exchangeDataSet1");
+//    // Fragmentary simulation for periodic/random save after some actions:
+//    Player player = new Player.Builder("Adrian", new BigDecimal(200000), Difficulty.EASY).build();
+//    Exchange exchange = FilehandlerExchange.getExchangeDataset("exchangeDataSet1");
+//
+//    exchange
+//        .getStockMap()
+//        .forEach(
+//            (s, stock) -> {
+//              System.out.println(s + ": " + stock);
+//            });
+//
+//    try {
+//      exchange.buy("HST", new BigDecimal(31.0), player).commit(player);
+//      exchange.buy("AAPL", new BigDecimal(31.0), player).commit(player);
+//      exchange.buy("MSFT", new BigDecimal(1.0), player).commit(player);
+//      exchange.sell("MSFT", new BigDecimal(1.0), player).commit(player);
+//    } catch (TransactionException e) {
+//      throw new RuntimeException(e);
+//    }
+//
+//    FilehandlerPlayer.savePlayerData(player, "src/main/resources/saves/testgetsaveslot");
+//
+//    Player savedPlayer =
+//        FilehandlerPlayer.getPlayerSavedData("src/main/resources/saves/testgetsaveslot");
 
-    exchange
-        .getStockMap()
-        .forEach(
-            (s, stock) -> {
-              System.out.println(s + ": " + stock);
-            });
-
-    try {
-      exchange.buy("HST", new BigDecimal(31.0), player).commit(player);
-      exchange.buy("AAPL", new BigDecimal(31.0), player).commit(player);
-      exchange.buy("MSFT", new BigDecimal(1.0), player).commit(player);
-      exchange.sell("MSFT", new BigDecimal(1.0), player).commit(player);
-    } catch (TransactionException e) {
-      throw new RuntimeException(e);
-    }
-
-    FilehandlerPlayer.savePlayerData(player, "src/main/resources/saves/testgetsaveslot");
-
-    Player savedPlayer =
-        FilehandlerPlayer.getPlayerSavedData("src/main/resources/saves/testgetsaveslot");
     launch(args);
   }
 
